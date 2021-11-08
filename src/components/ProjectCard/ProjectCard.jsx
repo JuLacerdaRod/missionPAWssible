7f2b3ed4ad06c8889f0572e3910872fd6b6b4111
Ju Lacerda <julacerdarod@gmail.com>
2021-11-08 14:53:39 +1000

    Fixed navbar

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -5,7 +5,7 @@ import "./ProjectCard.css";
 function ProjectCard(props) {
   const { projectData } = props;
   return (
-    <div className="card-wrapper">
+    <div>
       <div className="project-card">
         <Link to={`/project/${projectData.id}`}>
           <img alt="projectData" src={projectData.image} />
